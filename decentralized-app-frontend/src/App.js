ce31e3acd2ca11b1b674569cd395368ea4cea080
unknown <ariel18vola@gmail.com>
2022-04-09 19:54:21 -0300

    decentralized simple app demostration

---
+++ decentralized-app-frontend/src/App.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+import { gun } from './gun';
+
+function App() {
+
+  const [localMessage, setLocalMessage] = useState('');
+  const [messageList, setMessageList] = useState({});
+  const [showList, setShowList] = useState(false);
+
+  useEffect(() => {
+    gun
+      .get('Workbench')
+      .get('messages')
+      .map()
+      .on((message) => {
+        setShowList(false);
+        const newMessages = messageList;
+        newMessages[message.id] = message;
+        
+        setMessageList(newMessages);
+        setShowList(true);
+      })
+
+  }, [])
+  
+
+  const onChangeMessage = (event) => {
+    const value = event.target.value;
+    setLocalMessage( value );
+  }
+
+  const onSend = (event) => {
+    event && event.preventDefault();
+
+    const uid = uuidv4();
+
+    const newMessage = {
+      id: uid,
+      message: localMessage
+    };
+
+    gun.get('Workbench').get('messages').set(newMessage);
+    setLocalMessage('');
+
+  }
+
+
+  return (
+    <div className="App">
+        <h1>Send a message</h1>
+
+        <form onSubmit={ onSend }>
+          <input value={ localMessage } onChange={ onChangeMessage } />
+          <button onClick={ onSend }>Enviar</button>
+        </form>
+
+        <h2>Messages history</h2>
+        {
+          showList && Object.values(messageList).map((m) => {
+            return <p key={m.id}>{m.message}</p>
+          })
+        }
+    </div>
+  );
+}
+
+export default App;
---
+++ decentralized-app-frontend/src/gun.js
@@ -0,0 +1,6 @@
+import Gun from 'gun';
+
+const gun = Gun('http://localhost:8000/gun');
+
+
+export { gun };
